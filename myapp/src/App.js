0ada998390e20627b7320d1bb846b3bbb45889ac
Aman Prateek <amanpra333@gmail.com>
2019-06-05 12:20:27 +0530

    React Componet Part 2

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -3,17 +3,24 @@ import logo from './logo.svg';
 import {Navbar,NavbarBrand,Button} from 'reactstrap';
 import Menu from "./Components/MenuComponents";
 import './App.css';
+import {DISHES} from './shared/dishes';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state={
+      dishes: DISHES
+    }
+  }
   render() {
     return (
       <div>
        <Navbar dark color ='primary'>
          <div className='container'>
-           <NavbarBrand href='/'>Coursera</NavbarBrand>
+           <NavbarBrand href='/'>Dishes</NavbarBrand>
          </div>
        </Navbar>
-       <Menu />
+       <Menu dishes = {this.state.dishes} />
       </div>
     );
   }
